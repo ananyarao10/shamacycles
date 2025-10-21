2deddf666ebe5261e8e00a98c6b58463a91729d1
Ananya Rao <ananya.rao@gmail.com>
2025-10-21 14:37:04 -0500

    new project setup

--- shama-cycles/tailwind.config.js
+++ shama-cycles/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
